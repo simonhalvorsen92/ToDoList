bf459744f909b4fad7d514d992a38e185fed826e
simonhalvorsen92 <simon.halvorsen1@gmail.com>
2021-11-11 15:47:20 +0100

    mobile

--- js/main.js
+++ js/main.js
@@ -4,106 +4,71 @@ class ToDo {
     constructor(item){
         this.item = item;
         this.done = false;
-}
+    }
 } 
 
-
-
-
-
-// let Button = document.getElementById("lägTill");
-// let HärListanSkaPlaceras = document.getElementById("HärListanSkaPlaceras");
-// let skrivOmråde = document.getElementById("skrivOmråde");
-
-// Button.addEventListener("click", function(){
-//     let pTagg = document.createElement("p");
-//     pTagg.innerText = skrivOmråde.value;
-//     HärListanSkaPlaceras.appendChild(pTagg);
-    
-// });
-
-
-
-
-
-
-  
-//  let button = document.getElementById("button");
-//  let textRespond = document.getElementById("textRespond");
-//  let textArea = document.getElementById("textArea");
-
-//  button.addEventListener("click"), function () {
-//    let NewP = document.createElement("p")
-//    NewP.innerText = textArea.value;
-//    text - textRespond.appendChild(NewP);
-   
-//  }
-
-
-
-  // 2. Spara textarens value i en variabel (userText)
-//  let resultat=text.value
   
 
 
-  // 3. Hitta text-respond
 
-// let synlig=document.getElementById("text-respond");
-  // 4. Sätt innerHTML på text-respond till userText
-// synlig.innerHTML=resultat;
-// }
-
-// window.onload = function () {
-//     let personOne = new Person (180,"blue", "blond");
-// let personTwo = new Person (174,"green", "brown");
-// console.log(personOne);
-// console.log(personTwo);
-
-// }
-
-// window.onload =function () {
-//     animal();
-    
-  
-    
-// }
 let milk = new ToDo ("mjölk");
 let banan = new ToDo ("banan");
-let coffee = new ToDo ("kaffe");
+let coffee = new ToDo ("kaffe");// ToDo = class
 let cheese = new ToDo ("ost");
 
 
+let toDo = [milk, banan, coffee, cheese];
+
   // objekt i stället
   function createHtml() { 
-    newUl.innerHTML = "";// rensar ul
+    newUl.innerHTML = "";// rensar ul detta är = <ul></ul>
     for (let i = 0; i < toDo.length; i++){
-  let newLi = document.createElement ("li");
-  newLi.innerHTML = toDo[i].item;//Här hämtar jag this:item i objektet
+      let newLi = document.createElement ("li");
+      newLi.innerHTML = toDo[i].item;//toDo = listan [i] = positionen på listan  .item = är objectet innanför classen. använd punkter när det gäller object
+      //let ListAddEvent = document.getElementById("ListAddEvent");// hämtade ID i boddy
+      newLi.addEventListener("click", () => { clickFunction(i) });// annonyma functioner
+
+      if (toDo[i].done  === true) {
+        newLi.className = "done";
+        
+      }
+      newUl.appendChild(newLi);
+    
+    } 
+    
+  }
+
+  function clickFunction(position) {// ropar på annonyma funcktioner
+    
+    toDo[position].done = !toDo[position].done; // toDo = listan, [position] = plaseringen på listan,
+    // .done = boliansom är false. !toDo -||-  är motstaten till false
+
+    createHtml();// anropar på listan ovan 
+    
+    
+  }
   
-    
-  newUl.appendChild(newLi);
-    
-  }
-   
-    
-  }
-let toDo = [milk, banan, coffee, cheese];
 
 
 let newUl = document.createElement("ul");
 
-
-createHtml()
 createHtml()
 
+createHtml()
 
-document.body.appendChild(newUl);
+// document.body.appendChild(newUl);
 
-
-
-
-
+let theId = "myMain"
 
 
-//KOLLA W3 SKOL PÅ TODOLISTOR FÖRSÖK FÖRSTÅ SKRIV NER MOMENT PÅ PAPPERwindow.onload = function () {
+let myCntainer = document.createElement("div");//Här skapar jag en DIV element
+myCntainer.classList.add("container");// min DIV får en .class och heter container
+document.getElementById(theId).appendChild(myCntainer);//skapar min container innanför min body
+myCntainer.appendChild(newUl); // Innanför container lägger in min tidigare ul med dess li
+
+
+
+
+
+
   
